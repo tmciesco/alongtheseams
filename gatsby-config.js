22249ab5160cfdb7a2855cdf5721c7a45c14b486
Troy Ciesco <tmciesco@gmail.com>
2020-06-22 21:35:12 -0400

    add snipcart

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,18 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
 			},
 		},
 		{
 			resolve: `gatsby-plugin-netlify-cms`,
 			options: {
 				enableIdentityWidget: false,
+			},
+		},
+		{
+			resolve: "gatsby-plugin-snipcartv3",
+			options: {
+				apiKey: "ZDIyMTRjM2MtOTJmNi00Mjc5LWIyZTktM2IyOWYyMjM4ZjM5NjM3MjgzNjEzODY0OTgyNzg4",
 			},
 		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
