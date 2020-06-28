049b7b77b8cbb5e9c37545fb9b6484cee1f307ca
Troy Ciesco <tmciesco@gmail.com>
2020-06-28 11:52:41 -0400

    test order place

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -42,7 +42,7 @@ const ShopPage = () => {
 								className="snipcart-add-item"
 								data-item-id={product.id}
 								data-item-price={product.variants[0].price * 0.01}
-								data-item-url="/shop"
+								data-item-url="/"
 								data-item-description={product.description}
 								data-item-image={product.images[0].src}
 								data-item-name={product.title}
