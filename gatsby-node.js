9d473c477ba24a52bdb2d951608f7e8f8ab30f4f
Troy Ciesco <tmciesco@gmail.com>
2020-06-28 17:09:14 -0400

    reconfigure blog post location

--- gatsby-node.js
+++ gatsby-node.js
@@ -78,8 +78,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 		context: { products },
 	})
 
+	console.log("shop created")
+
 	createPage({
 		path: `/test`,
 		component: path.resolve("./src/templates/TestSourceTemplate.js"),
 	})
+	console.log("test created")
 }
