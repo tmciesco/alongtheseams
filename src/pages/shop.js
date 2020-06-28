822fec9cbc22d6cd8716b816a68a61300cfa1162
Troy Ciesco <tmciesco@gmail.com>
2020-06-28 12:18:44 -0400

    test order place

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -28,6 +28,25 @@ const ShopPage = () => {
 			<h1>Shop Gatsby Page Works</h1>
 			{loading ? <h4>loading products...</h4> : ""}
 			<div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", justifyItems: "center" }}>
+				<div>
+					<h2 style={{ textAlign: "center" }}>Test</h2>
+					<button
+						style={{ margin: "0 auto", display: "block" }}
+						className="snipcart-add-item"
+						data-item-id="test-product-id"
+						data-item-price="42.00"
+						data-item-url="/shop"
+						data-item-name="Test"
+						data-item-custom1-name="Color"
+						data-item-custom1-options="Solid White|Solid Black|Heather Grey|Solid Red"
+						data-item-custom1-value="Solid Red"
+						data-item-custom2-name="Size"
+						data-item-custom2-options="XS|SM|MD|LG|XL|2XL"
+						data-item-custom2-value="MD"
+					>
+						Add to cart
+					</button>
+				</div>
 				{products.map((product) => {
 					return (
 						<div key={product.id}>
